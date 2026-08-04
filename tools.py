1cd8257d5d573811a95417aa30c2e6fe123e3bcb
Linku <linku807@naver.com>
2026-08-04 02:19:07 +0000

    update tools.py

--- tools.py
+++ tools.py
@@ -5,7 +5,25 @@ class DiscordTools():
     def __init__(self, bot):
         self.bot = bot
 
-    async def getMessage(self, amount:int, channelId:str):
-        channel = await self.bot.fetch_channel(channelId)
-        messages = await channel.fetch_message(amount)
-        return messages
+    async def getMessage(self, channelId:int, amount:int):
+        if amount>250:
+            return "too many amount of message"
+        channel = self.bot.get_channel(channelId)
+        if not channel:
+            return "channel not found"
+        if channel is not discord.abc.Messageable:
+            return "the channel is not Messageable"
+        processed_messages = []
+        async for message in channel.history(limit = amount):
+            processed_messages.append(
+                {"messageID":message.id,
+                "author":{
+                   "id":message.author.id,
+                   "is_bot":message.author.bot,
+                   "name":message.author.global_name
+                   },
+                "message_content":message.content, 
+                "created_at": message.created_at
+                }
+            )
+        return processed_messages
